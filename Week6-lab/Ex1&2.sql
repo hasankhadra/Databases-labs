296cb2a9f92e777c090d16761caf97c333b4a2ec
hasankhadra <59062695+hasankhadra@users.noreply.github.com>
2021-03-02 20:20:16 +0300

    Update Ex1&2.sql

--- Week6-lab/Ex1&2.sql
+++ Week6-lab/Ex1&2.sql
@@ -5,7 +5,7 @@ FROM rental
 JOIN inventory ON rental.inventory_id = inventory.inventory_id
 RIGHT JOIN film ON inventory.film_id = film.film_id
 JOIN film_category ON film.film_id = film_category.film_id
-JOIN category ON category.name = 'Horror' OR category.name = 'Sci-fi'
+JOIN category ON category.name = 'Horror' OR category.name = 'Sci-Fi'
 WHERE inventory.film_id is null AND (film.rating = 'PG-13' OR film.rating = 'R');
 -- The most expensive step is matching the film.rating from the film table
 -- To optimize it, we can use a hash index on the rating attribute to make the query faster
